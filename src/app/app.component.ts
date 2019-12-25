863b52f74ecf971d7f244d62728bb5593c0dfe2c
Andrew Leung <andrew@ualberta.net>
2019-12-25 12:41:18 +0000

    FEATURE: Learning Angular - Directives: ngStyle
    Code for article:
    
    http://redgreenrepeat.com/2020/01/10/learning-angular-directives-ngstyle/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,28 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    today_date = new Date();  // the current date -> page
-    input_value = ''; // set by user input (onKey event); page -> onKey -> page
-    onKey(event: any) {
-	this.input_value = event.target.value; // += event.target.value + '   ' ;
-    };
-
-    two_way_value; // by Angular using: FormsModule
-
-    isDisabled=false;
-
-    submitEnabled = false;
-    setSubmit(event: any) {
-	this.submitEnabled = true;
-    };
-
-    humanValidated=false;
-    humanCheck(event: any) {
-	if (event.target.value == (2+3)) {
-	    this.humanValidated = true;
-	}
-	else {
-	    this.humanValidated = false;
-	}
-    }
+    showDog = 'visible';
 }
