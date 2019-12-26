61c1deba269ff49da119fa118bf808f2d3d0dfc5
Andrew Leung <andrew@ualberta.net>
2019-12-26 23:10:02 +0000

    FEATURE: Learning Angular - Directives ngFor
    Code for article: http://redgreenrepeat.com/2020/01/24/learning-angular-directives-ngfor/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,31 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    show_ngIf = false;
-    show_dog = false;
+    words = {
+	"cat": {
+	    "show": false,
+	    "url": "https://www.dropbox.com/s/ip7trvghw45n4yc/cat.jpeg?raw=1",
+	},
+	"dog": {
+	    "show": false,
+	    "url": "https://www.dropbox.com/s/ugeqlxvawfqwhth/dog_by_roaming_angel.jpeg?raw=1",
+	},
+	"cow": {
+	    "show": false,
+	    "url": "https://www.dropbox.com/s/h7kfnipb6ix1xxb/cow.jpeg?raw=1",
+	},
+	"ngIf": {
+	    "show": false,
+	    "url": "",
+	}
+    }
+
+    word_list = Object.keys(this.words)
+
     imageCheck(event: any) {
 	let input_string = event.target.value;
-
-	this.show_ngIf = /.*ngIf.*/.test(input_string);
-	this.show_dog = /.*dog.*/.test(input_string);
+	for(var word of this.word_list) {
+	    this.words[word].show = (new RegExp(word)).test(input_string)
+	}
     }
 }
