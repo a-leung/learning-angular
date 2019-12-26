80030c485ed815dd3abff0a4e4516e781ba4f407
Andrew Leung <andrew@ualberta.net>
2019-12-26 07:31:23 +0000

    FEATURE: code for ngIf article
    http://redgreenrepeat.com/2020/01/17/learning_angular_directives_ngif/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    showDog = 'visible';
+    show_ngIf = false;
+    show_dog = false;
+    imageCheck(event: any) {
+	let input_string = event.target.value;
+
+	this.show_ngIf = /.*ngIf.*/.test(input_string);
+	this.show_dog = /.*dog.*/.test(input_string);
+    }
 }
